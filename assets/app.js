b3eba9f982f59dfac467cfe95c4289fb3a8b13e5
Andrew Wybo <Andrew Wybo>
2017-09-02 13:02:53 -0400

    added layout for html, added beggining vars for js

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
 
-
+var key ="fde6c58ab10c4620bb10425b10b2032c"
+var moviegif=""
+var queryURL="https://api.giphy.com/v1/gifs/search?q=" moviegif + "&api_key=" + key + "&limit=10&rating=PG-13";
 })
 
