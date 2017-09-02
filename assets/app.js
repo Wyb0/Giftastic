6a463c542fbec43a38b3c2c5b8b91023b7a52851
Andrew Wybo <Andrew Wybo>
2017-09-02 16:10:00 -0400

    added working ajax

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,18 @@
 $(document).ready(function () {
 
-var key ="fde6c58ab10c4620bb10425b10b2032c"
-var moviegif=""
-var queryURL="https://api.giphy.com/v1/gifs/search?q=" moviegif + "&api_key=" + key + "&limit=10&rating=PG-13";
-})
+    //Array of Movies for initial buttons and to push new buttons to
+    var moviesArray = ["Pulp Fiction", "Reservoir Dogs", "Kill Bill", "Back to the Future", "Bill and Ted"]
 
+    var key ="fde6c58ab10c4620bb10425b10b2032c"
+    var moviegif=""
+    var queryURL="https://api.giphy.com/v1/gifs/search?q=" + moviegif + "&api_key=" + key + "&limit=10&rating=PG-13";
+   
+    $.ajax({
+        url: queryURL,
+        method: 'GET'
+    }).done(function (response) {
+        console.log(response)
+    })
+
+
+})
