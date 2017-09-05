f706cdbc7e80acfb92f23885191d30674c106086
Andrew Wybo <Andrew Wybo>
2017-09-05 12:20:37 -0400

    changed function for displaygifs and display buttons, working gifs with ratings

--- assets/app.js
+++ assets/app.js
@@ -1,21 +1,16 @@
-$(document).ready(function () {
-
-    //Array of Movies for initial buttons and to push new buttons to
+ //Array of Movies for initial buttons and to push new buttons to
     var moviesArray = ["Pulp Fiction", "Reservoir Dogs", "Kill Bill", "Back to the Future", "Bill and Ted"]
    
     function buttons() {
         $(".buttons").empty();
-        $.each(moviesArray, function (index) {
-            var b = $("<button>")
-            b.attr("data-name", moviesArray [index])
-            b.addClass("moviesAdd")
-            b.text(moviesArray[index])
-                $(".buttons").append(b)
-        })
-    }
-    //on click to post Gifs
-    $(document).on("click", ".moviesAdd", postGifs);
-        
+        for (var i = 0; i < moviesArray.length; i++) {
+            var b = $("<button>");
+            b.attr("data-name", moviesArray [i]);
+            b.addClass("moviesAdd");
+            b.text(moviesArray[i]);
+                $(".buttons").append(b);
+        }
+    };
 
     buttons();
 
@@ -25,29 +20,28 @@ $(document).ready(function () {
         moviesArray.push(movie);
         buttons();
         })
-
+    
     function postGifs () {
         var moviegif = $(this).attr("data-name")
         var key ="fde6c58ab10c4620bb10425b10b2032c"
         var queryURL="https://api.giphy.com/v1/gifs/search?q=" + moviegif + "&api_key=" + key + "&limit=10&rating=PG-13";
-        
         $.ajax({
         url: queryURL,
         method: 'GET'
         }).done(function (response) {
+            $(".moviegifs").empty();
             console.log(response)
-            var data = response.data;
-            for (var i = 0; i <data.length; i++) {
-                $("<div>").append(".movieGifs");
-                var gif = $("<img>");
-                $(".movieGifs").append(gif)
-                var animateGif = data[i].images.original.url;
-                var stillGif = data[i].images.original_still.url;
-                var ratings = data[i].rating;
-                var ratingPosted = $("<p>");
-                    ratingPosted.text("Rating = " + ratings);
-                    
+            for (var i = 0; i < response.data.length; i++) {
+                var animateGif = response.data[i].images.original.url;
+                var gifImage = $("<img>").attr("src", animateGif);
+                var stillGif = response.data[i].images.original_still.url;
+                var ratings = response.data[i].rating;
+                $(".moviegifs").append("<br>", "Rating = ", ratings, "<br>", gifImage, )
+            
             }
         })
     }
-});
+
+$(document).on("click", ".moviesAdd", postGifs);
+
+
