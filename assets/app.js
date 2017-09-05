ba89387247768e026cd42e1713bd36c8a2356569
Andrew Wybo <Andrew Wybo>
2017-09-04 23:43:50 -0400

    moved variables under function to get movie data working on backend, images posting not working

--- assets/app.js
+++ assets/app.js
@@ -2,10 +2,6 @@ $(document).ready(function () {
 
     //Array of Movies for initial buttons and to push new buttons to
     var moviesArray = ["Pulp Fiction", "Reservoir Dogs", "Kill Bill", "Back to the Future", "Bill and Ted"]
-
-    var key ="fde6c58ab10c4620bb10425b10b2032c"
-    var moviegif= $(this).attr("data-name")
-    var queryURL="https://api.giphy.com/v1/gifs/search?q=" + moviegif + "&api_key=" + key + "&limit=10&rating=PG-13";
    
     function buttons() {
         $(".buttons").empty();
@@ -17,6 +13,9 @@ $(document).ready(function () {
                 $(".buttons").append(b)
         })
     }
+    //on click to post Gifs
+    $(document).on("click", ".moviesAdd", postGifs);
+        
 
     buttons();
 
@@ -27,13 +26,28 @@ $(document).ready(function () {
         buttons();
         })
 
-    $.ajax({
+    function postGifs () {
+        var moviegif = $(this).attr("data-name")
+        var key ="fde6c58ab10c4620bb10425b10b2032c"
+        var queryURL="https://api.giphy.com/v1/gifs/search?q=" + moviegif + "&api_key=" + key + "&limit=10&rating=PG-13";
+        
+        $.ajax({
         url: queryURL,
         method: 'GET'
-    }).done(function (response) {
-        console.log(response)
-        
-    })
-
-
-})
+        }).done(function (response) {
+            console.log(response)
+            var data = response.data;
+            for (var i = 0; i <data.length; i++) {
+                $("<div>").append(".movieGifs");
+                var gif = $("<img>");
+                $(".movieGifs").append(gif)
+                var animateGif = data[i].images.original.url;
+                var stillGif = data[i].images.original_still.url;
+                var ratings = data[i].rating;
+                var ratingPosted = $("<p>");
+                    ratingPosted.text("Rating = " + ratings);
+                    
+            }
+        })
+    }
+});
